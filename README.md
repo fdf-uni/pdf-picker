df2a0f570ec2ab999cc8033b1a07c0085bb66813
fdf <171835561+fdf-uni@users.noreply.github.com>
2025-07-10 01:01:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# PDF Picker
+
+## What is this?
+This repository contains a relatively short and simple Python script which searches for all PDF files in a specified directory (by default `$HOME`) and then asks the user which one they want to open.
+If the selected PDF file has a table of contents in its metadata, the user is asked for a specific entry (unless disabled, see [below](#Usage)) at which the document is then opened.
+Otherwise, or if the selection is canceled, the document is simply opened directly without specifying any particular position.
+
+Personally, I have bound the execution of this script to a shortcut through my window manager (cp. "example usage" [below](#Usage)), so that I can always jump to any PDF rather quickly, but by default, it runs completely on the command line.
+
+## Caveats
+The extraction of the table of contents relies entirely on its existence within the metadata of the PDF.
+One can easily verify, whether any given document has one, by for example simply opening it in one's preferred PDF viewer and checking, whether there is a TOC in the sidebar or any other appropriate place within the UI (for `zathura` users, simply press `Tab`).
+
+In case that you have some documents which don't satisfy this requirement, but to which you'd like to add a table of contents, I can highly recommend [pdf.tocgen](https://github.com/Krasjet/pdf.tocgen)! :)
+
+## Installation and Dependencies
+
+Simply save the file `pdf_picker.py` wherever you want and install `pymupdf` using for example `pip install pymupdf`.
+
+### Hard Dependencies
+- `pymupdf` library (mainly used for extraction of TOC)
+
+### Soft Dependencies
+
+> Note: All these programs can be customized using command line arguments, see [Usage](#Usage), which is why I refer to them as soft dependencies.
+
+- `fd` (used for slightly faster search and mainly the ability to skip hidden files)
+    If `fd` is not found, the script automatically falls back to `find`, so if you prefer the latter, there's nothing you need to do. :D
+- `fzf`, the default selection command
+- `zathura`, the default PDF viewer
+
+## Usage
+
+```
+usage: pdf_picker.py [-h] [-f] [-nt] [-hi] [-b BASE_DIRECTORY] [-s SELECTOR]
+                     [-si] [-psa PDF_SELECTOR_ARGS] [-tsa TOC_SELECTOR_ARGS]
+                     [-p PDF_VIEWER] [-pa PDF_VIEWER_ARGS] [-mc]
+                     [--search-cmd SEARCH_CMD]
+
+Interactively open a PDF file, optionally at a specific TOC entry.
+
+options:
+  -h, --help            show this help message and exit
+  -f, --full-path       show full paths of the pdf files during selection
+  -nt, --no-toc         never ask for the preferred table of contents entry
+  -hi, --hidden         also search for hidden files (only relevant if `fd` is
+                        installed and `--search-cmd` is not used)
+  -b, --base-directory BASE_DIRECTORY
+                        base directory for searching pdf files
+  -s, --selector SELECTOR
+                        command to launch for selecting items. It needs to
+                        return the index of the selection, not the selection
+                        itself. If this is not possible, please refer to
+                        `--selector-indices`.
+  -si, --selector-indices
+                        preceed selection items by their indices
+  -psa, --pdf-selector-args PDF_SELECTOR_ARGS
+                        additional arguments to pass to the selector during
+                        pdf selection (if your arguments start with a dash,
+                        make sure to add a equal sign, i.e. `-psa="-option"`)
+  -tsa, --toc-selector-args TOC_SELECTOR_ARGS
+                        additional arguments to pass to the selector during
+                        toc entry selection (please take the same caveat as
+                        for `-psa` into account)
+  -p, --pdf-viewer PDF_VIEWER
+                        command to launch for viewing pdf files
+  -pa, --pdf-viewer-args PDF_VIEWER_ARGS
+                        arguments to optionally provide to the pdf viewer for
+                        opening at a specific position. These can contain the
+                        strings '$page', '$xloc' and '$yloc' which will be
+                        replaced accordingly, cp. "example usage" below.
+  -mc, --mupdf-coordinate-space
+                        pass coordinates in mupdf coordinate space to the pdf
+                        viewer instead of default pdf coordinate space
+  --search-cmd SEARCH_CMD
+                        command to launch for searching pdf files (overrides
+                        `--base-directory` and `--hidden`)
+
+example usage:
+    # Use rofi as the selector with custom prompts
+    python pdf_picker.py -s 'rofi -dmenu -i -format i' -psa='-p PDF' -tsa='-p TOC'
+
+    # Use fuzzel as a selector and sioyek as the pdf viewer
+    # Also show full paths and only search within the Documents directory
+    python pdf_picker.py -f -b ~/Documents  \
+        -s 'fuzzel -d --counter --index -w 100' \
+        -p 'sioyek' -mc -pa '--page $page --xloc $xloc --yloc $yloc'
+
+Have fun reading! :D
+```
+
+Please note the described requirement for the equals sign when providing `-psa` or `-tsa` with strings that start with a dash.
+This sadly seems to be a [limitation of Python's argparse library](https://github.com/python/cpython/issues/53580).
+
+`-si`/`--selection-indices` might seem a bit weird, but it is mainly used since file names without their full path might not be unique.
+Hence, we cannot rely only on the selected file name.
+This script alleviates this problem, by instead storing the PDF files with their full path in a list and asking for the index of the selection instead of the file name, so the selector needs to be able to return the index of the selection (this can be seen in the above example usages, where we add `-format i` to `rofi` and `--index` to `fuzzel`).
+However, some commands such as `fzf` might not provide this feature.
+When `-si` is used, all file names are passed to the selection command together with the corresponding indices preceding them.
+After a selection has occurred, the file name is then again stripped from the result, leaving only the wanted index.
+Especially for `fzf` this provides a satisfactory solution, since we can use `fzf --with-nth 2..` to hide the indices during selection.
---
+++ pdf-picker.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+
+import re
+import shlex
+import subprocess
+from os.path import expandvars
+
+import pymupdf
+
+
+def merge_strings(*strings: list[str]):
+    """
+    Merge strings which might be None.
+    """
+    strings = [s for s in strings if s is not None]
+    return " ".join(strings) if strings else None
+
+
+def send_error(error: str):
+    print(error)
+    try:
+        subprocess.run(["notify-send", error])
+    except FileNotFoundError:
+        pass
+    exit(1)
+
+
+def try_running_subprocess(
+    cmd: list[str],
+    not_found_error: str = "Command not found!",
+    input: str = None,
+):
+    try:
+        result = subprocess.run(
+            cmd, capture_output=True, text=True, input=input
+        )
+    except FileNotFoundError:
+        send_error(not_found_error)
+    return result
+
+
+def search_pdfs(base_directory: str, cmd: str = None, hidden: bool = False):
+    """
+    Search for PDFs in the specified base directory. By default, we
+    try to use `fd`. If it is not installed, we fall back to `find`.
+
+    Alternatively, the specific search command to be used can be
+    supplied as `cmd`.
+    """
+    if cmd is not None:
+        pdfs = try_running_subprocess(shlex.split(cmd)).stdout.split("\n")[:-1]
+    else:
+        try:
+            pdfs = subprocess.run(
+                [
+                    "fd",
+                    "--no-ignore",
+                    "--hidden" if hidden else "",
+                    "--type",
+                    "file",
+                    "--extension",
+                    "pdf",
+                    "--absolute-path",
+                    ".",
+                    base_directory,
+                ],
+                capture_output=True,
+                text=True,
+            ).stdout.split("\n")[:-1]
+        except FileNotFoundError:
+            pdfs = subprocess.run(
+                ["find", base_directory, "-type", "f", "-name", "*.pdf"],
+                capture_output=True,
+                text=True,
+            ).stdout.split("\n")[:-1]
+
+    return pdfs
+
+
+def select(items: list[str], cmd: str, indices=False, check=True):
+    """
+    Launch a user-selection for the list `items` using for example
+    fzf (this is the default), fuzzel or rofi. The precise command
+    can be supplied using `cmd`.
+
+    Make sure that the subprocess returns the index, not the string
+    of the selection or use `indices=True` in order to add indices
+    at the front of each item.
+    """
+    # fzf as the default selector
+    if cmd is None:
+        cmd = "fzf --with-nth 2.."
+        indices = True
+    if indices:
+        for i in range(len(items)):
+            items[i] = f"{i}\t{items[i]}"
+    selection = try_running_subprocess(
+        shlex.split(cmd),
+        not_found_error="Selector not found!",
+        input="\n".join(items),
+    ).stdout
+    if indices:
+        selection = re.match(r"\d*", selection)[0]
+    if selection == "":
+        if check:
+            send_error("Nothing selected!")
+            exit(1)
+        else:
+            selection = "-1"
+    return int(selection)
+
+
+def get_toc(path, mupdf_coordinate_space: bool = False):
+    """
+    Extract the table of contents from a PDF file.
+    """
+    with pymupdf.open(path) as doc:
+        toc = []
+        for t in doc.get_toc(simple=False):
+            page = doc.load_page(t[0])
+            # We try to access the coordinates of the entries at least
+            # once, even if we don't need to transform them to mupdf
+            # coordinates since this enables us to ensure that every
+            # entry has them.
+            try:
+                coords = t[3]["to"]
+            except KeyError:
+                coords = pymupdf.Point()
+
+            if mupdf_coordinate_space:
+                coords *= page.transformation_matrix
+            t[3]["to"] = coords
+            toc.append(t)
+    return toc
+
+
+def open_pdf(
+    path: str,
+    cmd: str,
+    position_args: str = None,
+    position: list[int] = None,
+):
+    """
+    Open a PDF, optionally at a specific position (consisting of a list
+    including the page number, x position and y position, in that order),
+    with the by `cmd` specified viewer.
+
+    The string `position_args` specifies how the position parameters will
+    be passed to the pdf viewer, by replacing the substrings "$page", "$xloc",
+    "$yloc" with the corresponding values.
+
+    The default viewer is zathura.
+    """
+    if cmd is None:
+        cmd = "zathura"
+        position_args = "-P $page"
+    pos_strings = [r"\$page", r"\$xloc", r"\$yloc"]
+    if position is None:
+        try_running_subprocess(
+            shlex.split(cmd) + [path],
+            not_found_error="PDF viewer not found!",
+        )
+    else:
+        for i in range(3):
+            position_args = re.sub(
+                pos_strings[i], str(position[i]), position_args
+            )
+        try_running_subprocess(
+            shlex.split(cmd) + [path] + shlex.split(position_args),
+            not_found_error="PDF viewer not found!",
+        )
+
+
+if __name__ == "__main__":
+    # A bit of argument parsing
+    import argparse
+
+    epilog = """example usage:
+    # Use rofi as the selector with custom prompts
+    python %(prog)s -s 'rofi -dmenu -i -format i' -psa='-p PDF' -tsa='-p TOC'
+
+    # Use fuzzel as a selector and sioyek as the pdf viewer
+    # Also show full paths and only search within the Documents directory
+    python %(prog)s -f -b ~/Documents  \\
+        -s 'fuzzel -d --counter --index -w 100' \\
+        -p 'sioyek' -mc -pa '--page $page --xloc $xloc --yloc $yloc'
+
+Have fun reading! :D"""
+    desc = "Interactively open a PDF file, optionally at a specific TOC entry."
+    parser = argparse.ArgumentParser(
+        description=desc,
+        epilog=epilog,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "-f",
+        "--full-path",
+        help="show full paths of the pdf files during selection",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-nt",
+        "--no-toc",
+        help="never ask for the preferred table of contents entry",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-hi",
+        "--hidden",
+        help="""also search for hidden files (only relevant if `fd` is
+        installed and `--search-cmd` is not used)""",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-b",
+        "--base-directory",
+        help="base directory for searching pdf files",
+    )
+    parser.add_argument(
+        "-s",
+        "--selector",
+        help="""command to launch for selecting items. It needs to return the
+        index of the selection, not the selection itself. If this is not
+        possible, please refer to `--selector-indices`.
+        """,
+    )
+    parser.add_argument(
+        "-si",
+        "--selector-indices",
+        help="preceed selection items by their indices",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-psa",
+        "--pdf-selector-args",
+        help="""additional arguments to pass to the selector during pdf
+        selection (if your arguments start with a dash, make sure to add
+        a equal sign, i.e. `-psa="-option"`)""",
+    )
+    parser.add_argument(
+        "-tsa",
+        "--toc-selector-args",
+        help="""additional arguments to pass to the selector during toc
+        entry selection (please take the same caveat as for `-psa` into
+        account)""",
+    )
+    parser.add_argument(
+        "-p", "--pdf-viewer", help="command to launch for viewing pdf files"
+    )
+    parser.add_argument(
+        "-pa",
+        "--pdf-viewer-args",
+        help="""arguments to optionally provide to the pdf viewer for opening
+        at a specific position. These can contain the strings '$page', '$xloc'
+        and '$yloc' which will be replaced accordingly, cp. "example usage"
+        below.""",
+    )
+    parser.add_argument(
+        "-mc",
+        "--mupdf-coordinate-space",
+        help="""pass coordinates in mupdf coordinate space to the pdf viewer
+        instead of default pdf coordinate space""",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--search-cmd",
+        help="""command to launch for searching pdf files
+        (overrides `--base-directory` and `--hidden`)""",
+    )
+    args = parser.parse_args()
+
+    # Find all PDFs in the specified directory
+    if (b := args.base_directory) is not None:
+        search_dir = b
+    else:
+        search_dir = expandvars("$HOME")
+    pdfs = search_pdfs(search_dir, cmd=args.search_cmd, hidden=args.hidden)
+    # Let the user select a specific file
+    pdf_items = [p if args.full_path else re.sub(r".*/", "", p) for p in pdfs]
+    selected_pdf = pdfs[
+        select(
+            pdf_items,
+            cmd=merge_strings(args.selector, args.pdf_selector_args),
+            indices=args.selector_indices,
+        )
+    ]
+
+    # Extract the TOC
+    toc = get_toc(selected_pdf, args.mupdf_coordinate_space)
+
+    # If the PDF has no TOC, simply open it, otherwise ask the user for the
+    # desired entry and open the file at the corresponding page.
+    # Also respect the users choice via the relevant flag.
+    if len(toc) == 0 or args.no_toc:
+        open_pdf(selected_pdf, cmd=args.pdf_viewer)
+    else:
+        # If the selection does not work properly, e.g. because the user
+        # cancels it, we simply open the PDF, without specifying a page.
+        toc_index = select(
+            [t[1] for t in toc],
+            cmd=merge_strings(args.selector, args.toc_selector_args),
+            indices=args.selector_indices,
+            check=False,
+        )
+        if toc_index >= 0:
+            toc_point = toc[toc_index][3]["to"]
+            toc_position = [toc[toc_index][2], toc_point.x, toc_point.y]
+            open_pdf(
+                selected_pdf,
+                cmd=args.pdf_viewer,
+                position_args=args.pdf_viewer_args,
+                position=toc_position,
+            )
+        else:
+            open_pdf(selected_pdf, cmd=args.pdf_viewer)
